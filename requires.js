1e9f8f4ec36d4cb3db5c68d5e83a468ae44b0613
Ben Orozco <benoror@gmail.com>
2017-11-18 00:35:18 -0600

    Chore: Create-react-app

--- requires.js
+++
@@ -1,1 +0,0 @@
-window.blockstack = require('blockstack')
---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+const blockstack = require('blockstack');
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    let isSignedIn = this.checkSignedInStatus();
+
+    this.state = {
+      isSignedIn,
+      person: isSignedIn && this.loadPerson()
+    }
+
+    this.handleSignIn = this.handleSignIn.bind(this)
+    this.handleSignOut = this.handleSignOut.bind(this)
+  }
+
+  checkSignedInStatus() {
+    if (blockstack.isUserSignedIn()) {
+      // showProfile(profile)
+      return true;
+    } else if (blockstack.isSignInPending()) {
+      blockstack.handlePendingSignIn().then(function(userData) {
+        window.location = window.location.origin
+      })
+      return false;
+    }
+  }
+
+  loadPerson() {
+    let profile = blockstack.loadUserData().profile
+
+    return new blockstack.Person(profile)
+  }
+
+  handleSignIn(event) {
+    event.preventDefault();
+    blockstack.redirectToSignIn()
+  }
+
+  handleSignOut(event) {
+    event.preventDefault();
+    blockstack.signUserOut(window.location.href)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Blockstack Create React App</h1>
+        </header>
+        <p style={{display: this.state.isSignedIn ? 'none' : 'block' }}>
+          <button onClick={this.handleSignIn}>
+            Sign-in with Blockstack
+          </button>
+        </p>
+        <p style={{display: !this.state.isSignedIn ? 'none' : 'block' }}>
+          <button onClick={this.handleSignOut}>
+            Sign-out
+          </button>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App;

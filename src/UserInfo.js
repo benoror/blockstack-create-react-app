8d220bacd6c33fdef305b636963e13d7c43ab291
Souhail M <souhail.merroun1996@gmail.com>
2019-01-09 18:52:15 +0100

    Static profile picture
    Found 19 vulnerabilities (4 low, 10 moderate, 5 high)

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -13,7 +13,7 @@ class UserInfo extends Component {
         <img class="avatar" src={
             this.props.user && this.props.user.image
               ? this.props.user.image[0].contentUrl
-              : "http://espace-icare.net/wp-content/uploads/2017/05/noprofile-big.png"
+              : "/noprofilepicture.png"
           } />
         <h1>{this.props.user && this.props.user.name}</h1>
       </div>
